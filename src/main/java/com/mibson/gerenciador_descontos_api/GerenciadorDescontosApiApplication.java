eeeffe07ec7430a2e8e6fb74a06943457e01243c
Mibson Ferreira <mibinhogit@gmail.com>
2026-05-06 15:24:50 -0300

    feat: setup inicial, modelagem de produtos com heranca e refatoracao do service

---
+++ src/main/java/com/mibson/gerenciador_descontos_api/GerenciadorDescontosApiApplication.java
@@ -0,0 +1,13 @@
+package com.mibson.gerenciador_descontos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDescontosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDescontosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mibson/gerenciador_descontos_api/GerenciadorDescontosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mibson.gerenciador_descontos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDescontosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
