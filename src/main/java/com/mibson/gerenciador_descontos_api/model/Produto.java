3ae1595f3fd3bb425f33586138fd8f21f0840324
Mibson Ferreira <mibinhogit@gmail.com>
2026-05-06 16:32:08 -0300

    feat: configura polimorfismo Jackson na classe Produto

--- src/main/java/com/mibson/gerenciador_descontos_api/model/Produto.java
+++ src/main/java/com/mibson/gerenciador_descontos_api/model/Produto.java
@@ -1,5 +1,7 @@
 package com.mibson.gerenciador_descontos_api.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.mibson.gerenciador_descontos_api.exceptions.NomeInvalidoException;
 import com.mibson.gerenciador_descontos_api.exceptions.PrecoInvalidoException;
 import jakarta.persistence.*;
@@ -11,6 +13,11 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "tipo")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ProdutoFisico.class, name = "FISICO"),
+        @JsonSubTypes.Type(value = ProdutoDigital.class, name = "DIGITAL")
+})
 public abstract class Produto {
 
     @Id
