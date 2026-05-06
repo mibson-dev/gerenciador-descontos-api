ab9c0569a7972f2c7c9fe07522e2b01abd65547a
Mibson Ferreira <mibinhogit@gmail.com>
2026-05-06 16:22:49 -0300

    feat: implementa endpoints de criacao e listagem no ProdutoController

--- src/main/java/com/mibson/gerenciador_descontos_api/controller/ProdutoController.java
+++ src/main/java/com/mibson/gerenciador_descontos_api/controller/ProdutoController.java
@@ -1,16 +1,25 @@
 package com.mibson.gerenciador_descontos_api.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mibson.gerenciador_descontos_api.model.Produto;
+import com.mibson.gerenciador_descontos_api.service.ProdutoService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
+@AllArgsConstructor
 public class ProdutoController {
+    private final ProdutoService produtoService;
 
     @GetMapping
-    public String testarRota() {
-        return "API está rodando";
+    public List<Produto> mostrarlistaDeProdutos() {
+        return produtoService.listarTodos();
+    }
+
+    @PostMapping
+    public Produto salvarProduto (@RequestBody Produto produto) {
+        return produtoService.save(produto);
     }
 }
